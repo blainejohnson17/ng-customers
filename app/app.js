3b2baefb0ba2ced6c6ba82f77dd2e03e6f1e4e6c
Blaine Johnson <blainejohnson17@gmail.com>
2014-11-23 20:55:37 -0700

    Moves view logic to views folders and creates routes

--- app/app.js
+++ app/app.js
@@ -1,5 +1,16 @@
 (function() {
   'use strict';
 
-  angular.module('customersApp', ['ngRoute']);
+  angular
+    .module('customersApp', ['ngRoute'])
+    .config(function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          controller: 'CustomersController',
+          templateUrl: '/app/views/customers.html'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    });
 })();
