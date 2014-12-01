c4c3426aae845cf132299d4a2c844528be68ee92
Blaine Johnson <blainejohnson17@gmail.com>
2014-11-30 22:50:03 -0700

    Use appSettings in customersController

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -3,10 +3,12 @@
 
   angular
     .module('customersApp')
-    .controller('CustomersController', ['$scope', 'customersFactory', function($scope, customersFactory) {
+    .controller('CustomersController', ['$scope', 'customersFactory', 'appSettings', function($scope, customersFactory, appSettings) {
       $scope.sortBy = 'name';
       $scope.reverse = 'false';
       $scope.customers = [];
+      $scope.title = appSettings.title;
+      $scope.version = appSettings.version;
       
       function init() {
         $scope.customers = customersFactory.getCustomers();
